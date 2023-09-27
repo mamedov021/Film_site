c20f11638f46f2b788c938f46b4ca70483e88975
Araz Məmmədov <101507193+mamedov021@users.noreply.github.com>
2023-09-27 23:42:33 +0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,20 @@
+const  toggleBtn = document.querySelector('.toggle_btn')
+const  toggleBtnIcon = document.querySelector('.toggle_btn i')
+const  dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBtn.onclick = function(){
+    dropDownMenu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
+//navbar end
+ 
+
+// Function to keep the dropdown open on hover
+ 
+ 
+//stert slider
+ 
---
+++ script.js
@@ -0,0 +1,151 @@
+var swiper = new Swiper(".my_swiper_1", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  
+  autoplay: true,
+   
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next_1  ",
+    prevEl: ".swiper-button-prev_1  ",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    798: {
+      slidesPerView: 3,
+    },
+    1115: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper_2 = new Swiper(".my_swiper_2", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  
+  autoplay: true,
+   
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next_2",
+    prevEl: ".swiper-button-prev_2",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    798: {
+      slidesPerView: 3,
+    },
+    1115: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper_3 = new Swiper(".my_swiper_3", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  
+  autoplay: true,
+   
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next_3  ",
+    prevEl: ".swiper-button-prev_3 ",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    798: {
+      slidesPerView: 3,
+    },
+    1115: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper_4 = new Swiper(".my_swiper_4", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  
+  autoplay: true,
+   
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next_4  ",
+    prevEl: ".swiper-button-prev_4  ",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    798: {
+      slidesPerView: 3,
+    },
+    1115: {
+      slidesPerView: 4,
+    },
+  },
+});
